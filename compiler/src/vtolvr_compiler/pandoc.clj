96768e65b16294ccadcb159b305600b6ad479fff
Daniel Leong <me@dhleong.net>
2019-08-01 14:56:54 -0700

    Improve PDF generation with title/subtitle; tweak formatting

--- compiler/src/vtolvr_compiler/pandoc.clj
+++ compiler/src/vtolvr_compiler/pandoc.clj
@@ -5,19 +5,40 @@
             [clojure.java.shell :refer [sh]])
   (:import (java.io InputStream ByteArrayInputStream SequenceInputStream)))
 
-(def pandoc-invocation
-  ["pandoc"
-   "--from=markdown"
-   "--toc"
-   "--number-sections"
-   "--top-level-division=part"
-   "-"])
+(def pandoc-opts
+  {:from "markdown"
+   :toc true
+   :number-sections true
+
+   ; causing weird indenting issue...
+   ;; :top-level-division "part"
+
+   :metadata {:title "VTOLVR Handbook"
+              :subtitle "Everything you need to know and more"}})
+
+(defn- format-opts [[k v]]
+  (let [key-str (str "--" (name k))]
+    (cond
+      (true? v) [key-str]
+
+      (map? v) (map (fn [[sub-key sub-val]]
+                      (str key-str "=" (name sub-key) ":" sub-val))
+                    v)
+
+      :else [(str key-str "=" v)])))
+
+(defn ->pandoc-invocation [opts]
+  (cons "pandoc"
+        (concat (mapcat format-opts opts)
+                ["-"])))
 
 (defn from-stream [^InputStream stream]
   (let [{:keys [exit out err]} (apply sh
-                                      (concat pandoc-invocation
-                                              ["--output=test.pdf"]
-                                              [:in stream]))]
+                                      (concat
+                                        (->pandoc-invocation
+                                          (assoc pandoc-opts
+                                                 :output "test.pdf"))
+                                        [:in stream]))]
     (if (= 0 exit)
       out
       (throw (RuntimeException.
