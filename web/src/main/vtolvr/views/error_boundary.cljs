b93e5fcfdac8eafe2b924a4eff76754f9d8f3e50
Daniel Leong <me@dhleong.net>
2019-08-09 10:45:38 -0400

    Improve the error boundary with auto-retry (and a manual retry)
    Also renders the full stack of the error. Auto-retry is (naturally)
    debug-only. I've switched to using the `getDerivedStateFromError`
    callback to suppress the annoying error log about it, and since
    that's apparently the "correct" place to update state for handling
    the error.

--- web/src/main/vtolvr/views/error_boundary.cljs
+++ web/src/main/vtolvr/views/error_boundary.cljs
@@ -1,20 +1,45 @@
 (ns vtolvr.views.error-boundary
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [vtolvr.util :refer-macros [fn-click]]))
+
+(when goog.DEBUG
+  ; in debug builds, we can auto-retry rendering error'd components
+  ; every time a load occurs
+  (def ^:private active-err-atoms (atom #{}))
+
+  (defn- ^:dev/after-load clear-errors []
+    (swap! active-err-atoms (fn [atoms]
+                              (doseq [a atoms]
+                                (reset! a nil))
+
+                              ; clear
+                              #{}))))
 
 (defn error-boundary [& _]
   (r/with-let [err (r/atom nil)]
     (r/create-class
-     {:display-name "Error Boundary"
+      {:display-name "Error Boundary"
 
-      :component-did-catch (fn [_this error info]
-                             (js/console.warn error info)
-                             (reset! err error))
+       :component-did-catch (fn [_this error info]
+                              (js/console.warn error info))
 
-      :reagent-render (fn [& children]
-                        (if-let [err @err]
-                          [:div.error
-                           [:h1 "Oops! Something went wrong"]
-                           [:div (str err)]]
+       :statics
+       #js {:getDerivedStateFromError (fn [error]
+                                        (when goog.DEBUG
+                                          ; enqueue the atom for auto-clearing
+                                          (swap! active-err-atoms conj err))
 
-                          (into [:<>] children)))})))
+                                        (reset! err error))}
+
+       :reagent-render (fn [& children]
+                         (if-let [e @err]
+                           [:div.error
+                            [:h1 "Oops! Something went wrong"]
+                            [:div [:a {:href "#"
+                                       :on-click (fn-click
+                                                   (reset! err nil))}
+                                   "Try again"]]
+                            [:pre (.-stack e)]]
+
+                           (into [:<>] children)))})))
 
