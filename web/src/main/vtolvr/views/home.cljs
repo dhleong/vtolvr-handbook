8725304db394b85b26c2bf4472780d38fab9711c
Daniel Leong <me@dhleong.net>
2019-08-10 18:16:06 -0400

    Add download link for the PDF

--- web/src/main/vtolvr/views/home.cljs
+++ web/src/main/vtolvr/views/home.cljs
@@ -1,14 +1,28 @@
 (ns vtolvr.views.home
-  (:require [vtolvr.util :refer [<sub]]
+  (:require [spade.core :refer [defattrs]]
+            [vtolvr.config :as config]
+            [vtolvr.util :refer [<sub]]
             [vtolvr.views.widgets :refer [link]]))
+
+(defattrs home-styles []
+  {:padding "12px"}
+
+  [:.pdf-links {:padding "8px"}]
+
+  [:.section-links {:padding "8px"}])
 
 (defn view []
   (let [intro (<sub [:intro])
         sections (<sub [:index/sections])]
-    [:div.home
+    [:div.home (home-styles)
      [:div.intro intro]
 
-     [:div
+     [:div.pdf-links
+      [:a {:href (str config/site-prefix "vtolvr-handbook-en.pdf")
+           :download true}
+       "Download PDF"]]
+
+     [:div.section-links
       [:b "Available sections:"]
       [:ul
        (for [[id {title :title}] sections]
