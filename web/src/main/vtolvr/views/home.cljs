7f8d4aaae9ff125742a573b2e8562839612dedb5
Daniel Leong <me@dhleong.net>
2019-08-10 21:41:12 -0400

    Add a github-link widget

--- web/src/main/vtolvr/views/home.cljs
+++ web/src/main/vtolvr/views/home.cljs
@@ -1,7 +1,8 @@
 (ns vtolvr.views.home
-  (:require [spade.core :refer [defattrs]]
+  (:require [spade.core :refer [defattrs defclass]]
             [vtolvr.config :as config]
             [vtolvr.util :refer [<sub]]
+            [vtolvr.views.github-link :refer [github-link]]
             [vtolvr.views.widgets :refer [link]]))
 
 (defattrs home-styles []
@@ -14,20 +15,23 @@
 (defn view []
   (let [intro (<sub [:intro])
         sections (<sub [:index/sections])]
-    [:div.home (home-styles)
-     [:div.intro intro]
+    [:<>
+     [github-link]
 
-     [:div.pdf-links
-      [:a {:href (str config/site-prefix "vtolvr-handbook-en.pdf")
-           :download true}
-       "Download PDF"]]
+     [:div.home (home-styles)
+      [:div.intro intro]
 
-     [:div.section-links
-      [:b "Available sections:"]
-      [:ul
-       (for [[id {title :title}] sections]
-         ^{:key id}
-         [:li
-          [link {:href (str "section/" (name id))} title]])]]
-     ]))
+      [:div.pdf-links
+       [:a {:href (str config/site-prefix "vtolvr-handbook-en.pdf")
+            :download true}
+        "Download PDF"]]
+
+      [:div.section-links
+       [:b "Available sections:"]
+       [:ul
+        (for [[id {title :title}] sections]
+          ^{:key id}
+          [:li
+           [link {:href (str "section/" (name id))} title]])]]
+      ]]))
 
